b4674dbadb18b7489f1ffc7193589d779beab229
h.matsuo <matsuoshi@gmail.com>
2022-04-30 22:57:36 +0900

    post detail page

---
+++ src/lib/markdownToHtml.ts
@@ -0,0 +1,13 @@
+import { remark } from "remark"
+import html from "remark-html"
+
+/**
+ * @param markdown Markdown ファイル名
+ * @returns HTML
+ */
+const markdownToHtml = async (markdown: string) => {
+  const result = await remark().use(html).process(markdown)
+  return result.toString()
+}
+
+export default markdownToHtml
