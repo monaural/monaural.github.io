da2f1e8a6cce8f9711cc79aa4b3f1200c300b5d4
h.matsuo <matsuoshi@gmail.com>
2022-05-03 11:21:02 +0900

    add favicon and og:image

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,16 +2,25 @@ import Head from 'next/head'
 
 export default function Header({params}: {params: any}) {
   params = Object.assign({
-    title: 'stpo',
+    title: '',
     description: 'ストッポ',
-    image: 'https://example.com'
+    image: 'https://matsuoshi.github.io/img/stpo.png', // todo
+    url: 'https://example.com', // todo
+    type: false ? 'website' : 'article' // todo
   }, params)
+
+  params.title += (params.title ? ' | ' : '') + 'stpo'
 
   return (
     <Head>
       <title>{params.title}</title>
-      <meta name="description" content="{params.description}" />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content={params.description} />
+      <meta name="og:url" content={params.url} />
+      <meta name="og:title" content={params.title} />
+      <meta name="og:description" content={params.description} />
+      <meta name="og:image" content={params.image} />
+      <meta name="og:type" content={params.type} />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css"></link>
     </Head>
   )
