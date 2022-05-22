1786339851503d1e78de3f78065fdae1fe66f453
h.matsuo <matsuoshi@gmail.com>
2022-05-22 10:22:19 +0900

    GTM / GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from 'next/app'
+import { googleTagManagerId } from '../utils/gtm'
+import GTM, {GoogleTagManagerId} from '../components/GTM';
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <>
+    <GTM
+      GTMId={googleTagManagerId as GoogleTagManagerId}
+    />
+    <Component {...pageProps} />
+  </>
+)
 
 export default MyApp
